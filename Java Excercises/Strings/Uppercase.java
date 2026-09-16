55d0e15b7a3c6281e744f3e9cd0a772987a8f233
vaishnavichavanAC <vaishnavi.chavan.cmaug26@gmail.com>
2026-09-16 23:34:03 +0530

    Add files via upload

---
+++ Java Excercises/Strings/Uppercase.java
@@ -0,0 +1,11 @@
+public class Uppercase {
+    public static void main(String[] args) {
+       
+    	String str = "The Quick BroWn FoX!";
+    	System.out.println("Original String :" +str);
+    	
+    	String result=  str.toUpperCase();
+    	System.out.println("String in uppercase :" +result);
+        
+            }
+}
