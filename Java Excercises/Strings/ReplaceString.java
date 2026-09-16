fa87af01ded379ee8d2bab9d455a501a3f92f618
vaishnavichavanAC <vaishnavi.chavan.cmaug26@gmail.com>
2026-09-16 23:50:24 +0530

    Add files via upload

---
+++ Java Excercises/Strings/ReplaceString.java
@@ -0,0 +1,17 @@
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+	
+		
+		String str = "The quick brown fox jumps over the lazy dog";
+		System.out.println("Original String :" + str);
+		
+		String result = str.replace("fox", "cat");
+		
+		System.out.println("String in uppercase: " +result);
+		
+
+	}
+
+}
