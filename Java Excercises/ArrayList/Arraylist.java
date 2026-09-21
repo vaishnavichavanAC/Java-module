a4689003e18fc2b2797161a74fb3148da481f376
vaishnavichavanAC <vaishnavi.chavan.cmaug26@gmail.com>
2026-09-21 12:36:08 +0530

    Add files via upload

---
+++ Java Excercises/ArrayList/Arraylist.java
@@ -0,0 +1,27 @@
+package org.Vaishnavi;
+import java.util.ArrayList;
+public class Arraylist {
+
+	public static void main(String[] args) {
+
+//		1. Write a Java program to create a new array list, add some colors (string) and print out the collection.
+
+
+		ArrayList List = new ArrayList();
+		ArrayList<String> ArrayObj = new ArrayList<String>();
+		
+		ArrayObj.add("Pink");
+		ArrayObj.add("Blue");
+		ArrayObj.add("Green");
+		ArrayObj.add("Red");
+		
+		
+		
+		System.out.println(ArrayObj);
+		
+		
+		
+		
+	}
+
+}
