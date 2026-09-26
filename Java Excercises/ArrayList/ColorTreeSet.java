ce17ab2a840b1d8ca3cb1b46199e3aec9febecb7
vaishnavichavanAC <vaishnavi.chavan.cmaug26@gmail.com>
2026-09-26 13:59:13 +0530

    Add files via upload

---
+++ Java Excercises/ArrayList/ColorTreeSet.java
@@ -0,0 +1,25 @@
+
+package org.Vaishnavi;
+
+import java.util.TreeSet;
+
+public class ColorTreeSet {
+	
+
+    public static void main(String[] args) {
+
+        // Create a new TreeSet
+        TreeSet<String> colors = new TreeSet<String>();
+
+        // Add colors
+        colors.add("PINK");
+        colors.add("PURPLE");
+        colors.add("GREEN");
+        colors.add("BLACK");
+        colors.add("YELLOW");
+
+        // Print the TreeSet
+        System.out.println("TreeSet of colors: " + colors);
+    }
+}
+
